702d3fc682aef5eb9618815d44b174de6a7cbed4
FurkanÇetinkaya <gojucu@gmail.com>
2019-11-07 16:32:49 +0300

    onbeforeunload ile db ye son süreyi kaydedebiliyorum
    Stabil gibi şimdilik tekrar kontrol et

---
+++ VideoPlayerDenemeVideojs/VideoPlayerDenemeVideojs/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoPlayerDenemeVideojs.Models;
+namespace VideoPlayerDenemeVideojs.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        AkademinyaEntities db = new AkademinyaEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string deger)
+        {
+            UyeVideo uv = db.UyeVideo.FirstOrDefault(x => x.Id == 1);
+            uv.KalinanZaman = deger;
+            db.SaveChanges();
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
